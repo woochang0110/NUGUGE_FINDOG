bd42ac9509ac6129191b029545f37cb5f2ddf954
ygpark <dydrms3456@naver.com>
2023-12-20 10:07:25 +0900

    코드 최적화

--- dog.py
+++ dog.py
@@ -3,21 +3,11 @@ import numpy as np
 from pathlib import Path
 import matplotlib.pyplot as plt
 import openvino as ov
-
-
-
-
 import ipywidgets as widgets
-
-
-
 
 model_name = "v3-small_224_1.0_float"
 model_xml_path = f'{model_name}.xml'
 model_bin_path = f'{model_name}.bin'
-
-
-
 
 core = ov.Core()
 device = widgets.Dropdown(
@@ -29,42 +19,21 @@ device = widgets.Dropdown(
 model = core.read_model(model=model_xml_path)#model_xml_path
 compiled_model = core.compile_model(model=model, device_name=device.value)
 
-
-
-
 output_layer = compiled_model.output(0)
-
-
-
 
 # Load YOLO model configuration and weights
 net = cv2.dnn.readNet("yolov3.cfg", "yolov3.weights")
 net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
 net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
 
-
-
-
 # Load COCO class names
 classes = []
 with open("coco.names", "r") as f:
    classes = [line.strip() for line in f.readlines()]
 
-
-#
-
-# Load classification image
-img = cv2.imread("sheps.jpg")#dogs.jpg       #dogcat.png
+# Load image
+img = cv2.imread("dogcat.png")#dogs.jpg       #dogcat.png
 image = cv2.cvtColor(img, code=cv2.COLOR_BGR2RGB)
-# Resize to MobileNet image shape.
-input_image = cv2.resize(src=image, dsize=(224, 224))
-
-# Reshape to model input shape.
-input_image = np.expand_dims(input_image, 0)
-
-#do inference
-result_infer = compiled_model([input_image])[output_layer]#input image를 model에 넣고 output layer 배열의 어떤 요소인지
-result_index = np.argmax(result_infer)
 
 #classification
 imagenet_filename = 'imagenet_2012.txt'
@@ -73,40 +42,21 @@ imagenet_filename = 'imagenet_2012.txt'
 with open(imagenet_filename, "r") as f:
    imagenet_classes = f.read().splitlines()
 
-
 imagenet_classes = ['background'] + imagenet_classes
 
-
-print(imagenet_classes[result_index])
-dog_name=imagenet_classes[result_index]
-
-dog_name=dog_name.split()
-
+#화면 출력용 resize
 img = cv2.resize(img, None, fx=1.5, fy=1.5)
 height, width, channels = img.shape
-
-
-
-
-
-
-
 
 # Detecting objects
 blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
 net.setInput(blob)
 outs = net.forward(net.getUnconnectedOutLayersNames())
 
-
-
-
 # Information to display
 class_ids = []
 confidences = []
 boxes = []
-
-
-
 
 for out in outs:
    for detection in out:
@@ -128,14 +78,9 @@ for out in outs:
            confidences.append(float(confidence))
            class_ids.append(class_id)
 
-
-
-
 # Draw bounding boxes and labels
 font = cv2.FONT_HERSHEY_PLAIN
 colors = np.random.uniform(0, 255, size=(len(classes), 3))
-
-
 
 # Apply non-maximum suppression
 indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
